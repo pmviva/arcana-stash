b5b5ed1ce3f7cad526bb4efdb9c0e933d8f96f6a
Pablo Martin Viva <pmviva@gmail.com>
2025-05-06 01:32:18 -0300

    Updated auditable entity

--- arcana-stash-api/src/main/java/com/github/pmviva/arcana/stash/api/model/AuditableEntity.java
+++ arcana-stash-api/src/main/java/com/github/pmviva/arcana/stash/api/model/AuditableEntity.java
@@ -21,7 +21,35 @@
 
 package com.github.pmviva.arcana.stash.api.model;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
-public abstract class AuditableEntity extends BaseEntity {}
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AuditableEntity extends BaseEntity {
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
