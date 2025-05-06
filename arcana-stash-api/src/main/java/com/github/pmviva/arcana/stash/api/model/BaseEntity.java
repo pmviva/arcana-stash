0f02a01d03bf0a97dcc4e9b3a23f8310273d4589
Pablo Martin Viva <pmviva@gmail.com>
2025-05-06 01:27:52 -0300

    Updated base entity

--- arcana-stash-api/src/main/java/com/github/pmviva/arcana/stash/api/model/BaseEntity.java
+++ arcana-stash-api/src/main/java/com/github/pmviva/arcana/stash/api/model/BaseEntity.java
@@ -32,4 +32,12 @@ public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
 }
