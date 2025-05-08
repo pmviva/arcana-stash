b59c6e564dbc524cc8673075596f30c176ed9ca4
Pablo Martin Viva <pmviva@gmail.com>
2025-05-08 01:22:40 -0300

    feat(backend): update show

--- arcana-stash-api/src/main/java/com/github/pmviva/arcana/stash/api/model/Show.java
+++ arcana-stash-api/src/main/java/com/github/pmviva/arcana/stash/api/model/Show.java
@@ -21,7 +21,80 @@
 
 package com.github.pmviva.arcana.stash.api.model;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.time.LocalDate;
 
 @Entity
-public class Show extends AuditableEntity {}
+@Table(name = "shows")
+public class Show extends AuditableEntity {
+    /**
+     * Stores the name of the show
+     */
+    private String name;
+
+    /**
+     * Stores the description of the show
+     */
+    private String description;
+
+    /**
+     * Stores the date of the show
+     */
+    @Temporal(TemporalType.DATE)
+    private LocalDate showDate;
+
+    /**
+     * Stores the venue of the show
+     */
+    @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name = "name", column = @Column(name = "venue_name")),
+        @AttributeOverride(name = "address", column = @Column(name = "venue_address")),
+        @AttributeOverride(name = "city", column = @Column(name = "venue_city")),
+        @AttributeOverride(name = "state", column = @Column(name = "venue_state")),
+        @AttributeOverride(name = "country", column = @Column(name = "venue_country"))
+    })
+    private Venue venue;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getShowDate() {
+        return showDate;
+    }
+
+    public void setShowDate(LocalDate showDate) {
+        this.showDate = showDate;
+    }
+
+    @SuppressFBWarnings({"EI_EXPOSE_REP2", "EI_EXPOSE_REP"})
+    public Venue getVenue() {
+        return venue;
+    }
+
+    @SuppressFBWarnings({"EI_EXPOSE_REP2", "EI_EXPOSE_REP"})
+    public void setVenue(Venue venue) {
+        this.venue = venue;
+    }
+}
